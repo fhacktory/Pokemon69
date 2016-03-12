d98c52682306105a0838065da0c6743a719e7e5d
gsaynac <gildas@nelio.io>
2016-03-12 19:42:04 +0000

    Basic request on Yelp API

--- podium/api.py
+++ podium/api.py
@@ -1,16 +1,34 @@
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+import io
+import json
 
 def get_info(city, job):
-
     raw_fb = get_facebook(city, job)
     raw_yelp = get_yelp(city, job)
     raw = merge_api(raw_fb, raw_yelp)
     return raw
 
-def get_facebook(city, job, raw):
-    return raw
+def get_facebook(city, job):
+    return []
 
-def get_yelp(city, job, raw):
-    return raw
+def get_yelp(city, job):
+    print(city, job)
+    with io.open('yelp.json') as cred:
+        creds = json.load(cred)
+        auth = Oauth1Authenticator(**creds)
+        client = Client(auth)
+    results = client.search(city, **{
+        'term': job,
+        'lang': 'fr'
+    }).businesses
+    res = []
+    for result in results:
+        res.append({
+            "name": result.name,
+            "rating": result.rating
+        })
+    return res
 
 def merge_api(f, y):
     return y + f
